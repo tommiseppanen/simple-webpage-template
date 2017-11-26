ad027fcb0fc6008e0e59205c9e263b81cc16f429
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2017-11-26 21:06:11 +0200

    Added PostCSS responsive properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     plugins: [
+        require('postcss-import')({
+            plugins: [
+                require('postcss-responsive-properties')
+            ],
+        }),
         require('precss'),
         require('autoprefixer'),
-		require('lost')
+        require('lost')
     ]
 }
